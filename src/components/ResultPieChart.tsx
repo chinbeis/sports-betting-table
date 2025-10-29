41ddd8a36f6950644a30ea2f1931046a17bb1657
Chinbeis <chinbeis0705@gmail.com>
2025-10-29 18:18:54 +0800

    eslint fixes

--- src/components/ResultPieChart.tsx
+++ src/components/ResultPieChart.tsx
@@ -3,7 +3,7 @@
 import * as am5 from "@amcharts/amcharts5";
 import * as am5percent from "@amcharts/amcharts5/percent";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
-import { useEffect, useLayoutEffect } from "react";
+import { useLayoutEffect } from "react";
 
 interface ResultPieChartProps {
   data: { name: string; value: number }[];
